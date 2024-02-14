372fd23c2bdf74ab0fd971c73daf0dd8e745610b
MayFoxes <145255404+MayFoxes@users.noreply.github.com>
2024-02-14 16:18:33 +0500

    fix to checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidatorException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidatorException.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.exception;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ValidatorException extends IllegalArgumentException{
+public class ValidatorException extends IllegalArgumentException {
     public ValidatorException(final String message) {
         log.error(message);
     }
