644d41ebb681eacb6432edf6c8b1e598bfa231ee
FoxInc <nikita.myasnikoff@yandex.ru>
2024-03-28 19:28:43 +0500

    HW-11 feat: add database

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Likes.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+public class Likes {
+
+    @NotNull
+    private Integer id;
+
+    @NotNull
+    private Integer userId;
+
+    @NotNull
+    private Integer filmId;
+}
