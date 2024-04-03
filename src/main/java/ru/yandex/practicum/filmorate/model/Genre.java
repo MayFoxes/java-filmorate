12d78bf896dbeed11f5ec91113f5339a43fe1f30
FoxInc <nikita.myasnikoff@yandex.ru>
2024-04-03 20:24:13 +0500

    Fixed

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -2,13 +2,11 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
-@EqualsAndHashCode
 public class Genre {
 
     @NotNull
