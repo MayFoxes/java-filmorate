6b2602689e17b8e2f7dbcdfbd1374d8dcf4ed5a0
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-04-04 12:04:55 +0300

    feat: добавить приватное неизменяемое поле filmStorage для вызова метода getUserRecommendations. Добавить сам метод getUserRecommendations для получения рекомендаций фильмов для пользователя

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserServiceImpl.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.filmorate.service.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Collection;
@@ -10,7 +12,7 @@ import java.util.Collection;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
+    private final FilmStorage filmStorage;
     private final UserStorage userStorage;
 
     @Override
@@ -56,5 +58,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public Collection<User> getCommonFriends(Integer id, Integer otherId) {
         return userStorage.getCommonFriends(id, otherId);
+    }
+
+    @Override
+    public Collection<Film> getUserRecommendations(Integer userId) {
+        return filmStorage.getUserRecommendations(userId);
     }
 }
