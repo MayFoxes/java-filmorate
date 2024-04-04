b96648e832bc54e491831c8aed904f503e7da49e
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-04-04 12:01:55 +0300

    feat: добавить эндпоинт получения рекомендаций фильмов для пользователя getUserRecommendations

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.user.UserService;
 
@@ -58,5 +59,10 @@ public class UserController {
     @DeleteMapping("/{id}/friends/{friendId}")
     public void deleteFriend(@PathVariable Integer id, @PathVariable Integer friendId) {
         userService.deleteFriend(id, friendId);
+    }
+
+    @GetMapping("/{id}/recommendations")
+    public Collection<Film> getUserRecommendations(@PathVariable("id") Integer userId) {
+        return userService.getUserRecommendations(userId);
     }
 }
